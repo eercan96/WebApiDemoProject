fcb00f339450b3d5ff0b19449a928a990f2bcc49
Emre <emreercan755@gmail.com>
2025-05-02 17:34:25 +0300

    Delete-GetList-Add

---
+++ WebApiDemoProject/Controllers/SepetimController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApiDemoProject.Model;
+
+namespace WebApiDemoProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SepetimController : ControllerBase
+    {
+        Context context =new Context();
+
+        [HttpGet("[action]")]
+        public IActionResult GetList()
+        {
+            var result=context.Sepetim.ToList();
+            return Ok(result);
+        }
+
+        [HttpPost("[action]")]
+        public IActionResult Add(Sepet sepet) 
+        { 
+            
+            context.Sepetim.Add(sepet);
+            context.SaveChanges();
+            return Ok("Sepete ürün eklendi");
+
+        }
+
+        [HttpGet("[action]/{id}")]
+        public IActionResult Delete(int id)
+        {
+            var result =context.Sepetim.FirstOrDefault(x => x.Id == id);
+            context.Sepetim.Remove(result);
+            context.SaveChanges();
+            return Ok("Ürün silindi");
+        }
+    }
+}
--- WebApiDemoProject/Controllers/UrunlerimController.cs
+++ WebApiDemoProject/Controllers/UrunlerimController.cs
@@ -10,13 +10,30 @@ namespace WebApiDemoProject.Controllers
     {
         Context context=new Context();
 
-        [HttpGet]
+        [HttpGet("GetList")]
         public IActionResult GetList()
         {
             List<Urun> urunlerim = new List<Urun>();
             urunlerim=context.Urunler.ToList();
             return Ok(urunlerim);
 
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(Urun urun)
+        {
+            context.Urunler.Add(urun);
+            context.SaveChanges();
+            return Ok("İşlem Tamam");
+        }
+
+        [HttpGet("Delete")]
+        public IActionResult Delete(int id) 
+        {
+            var result = context.Sepetim.Where(u => u.Id == id).FirstOrDefault();
+            context.Remove(result);
+            context.SaveChanges();
+            var list=context.Sepetim.ToList();
+            return Ok(list);
         }
      
 
